8ec3c828794fc3eec8fe6f361c77f33f9f26bffa
GustavoMaiaOliveira <gustavomaia334@gmail.com>
2025-08-05 14:45:55 -0300

    first commit

---
+++ teste.py
@@ -0,0 +1,39 @@
+print('teste')
+
+nome = 'maia'
+cor_favorita = 'roxo'
+
+print(nome, cor_favorita)
+
+saldo = 5000
+
+print(saldo)
+
+saldo = saldo + 2000
+
+print('Seu saldo é de R$', saldo)
+
+saldo = saldo -500
+
+print('Esse é seu saldo atual', saldo)
+
+saldo = saldo * 2
+
+print('Esse é seu saldo dobrado', saldo)
+
+import this
+
+a = 500
+
+tenis = 500
+
+idade = 18
+
+# se a idade for mais que 18 anos, pode dirigir
+# se não
+# não pode dirigir
+
+if idade >= 18:
+    print('Pode dirigir!')
+else:
+    print('Não pode dirigir!')
